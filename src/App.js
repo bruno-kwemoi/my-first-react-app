7eab58d4a76000070883f7361835a675ead12cc7
bruno-kwemoi <bsaekwo@gmail.com>
2023-04-23 15:41:44 +0300

    Initial commit of all files to the repository

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import {useEffect, useState} from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
+
+
+const API_URL = 'https://www.omdbapi.com?apikey=cb68e3a0';
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+
+    const [searchTerm, setsearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect (() => {
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Entertainmet Hub</h1>
+            <h2>Search for your favorite movies</h2>
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setsearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                < MovieCard movie={movie } />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+            
+        </div>
+    );
+}
+
+
+export default App;
